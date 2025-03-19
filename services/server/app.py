d6aef46ed897c4e8cd33ed522e644952d13d1880
Eleazar-ZOUBGA <eleazarzoubga@gmail.com>
2025-03-19 22:00:13 +0100

    Correction des erreurs dans les fonctions d'authentification et d'inscription

--- services/server/app.py
+++ services/server/app.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from routes.chat import chat_blueprint
 from routes.base import base_blueprint
-from routes.auth import auth
+from routes.auth import auth_blueprint
 app = Flask(__name__)
 
 CORS(app)
 
 app.register_blueprint(base_blueprint)
 app.register_blueprint(chat_blueprint)
-app.register_blueprint(auth, url_prefix='/auth')
+app.register_blueprint(auth_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, threaded=True)
